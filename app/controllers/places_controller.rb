3effa177182ca5790cbeabd9d03f3a3e9e980079
Ubuntu12 <hugo@ubuntu.(none)>
2014-05-01 21:52:49 -0700

    commit2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
 	def index
+		if current_user
 		@foursquare_user = User.find(session[:user_id]) if session[:user_id]
 		@client = Foursquare2::Client.new(:oauth_token => @foursquare_user.token,:api_version => '20131231' ) if @foursquare_user
-
+	end
 	end
 end
